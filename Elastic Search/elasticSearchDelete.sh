894363b35d37d46118182760072feb173a8ff1ab
Jain-Pratiksha <74486549+Jain-Pratiksha@users.noreply.github.com>
2023-03-25 15:10:16 +0530

    Delete Documents

---
+++ Elastic Search/elasticSearchDelete.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# specify the Elasticsearch endpoint and index name
+endpoint="base_url"
+index_name="index_name"
+username="username"
+password="password"
+
+# specify the Elasticsearch query to retrieve all documents
+#Set the time as per you
+query='{
+   "query": {
+      "bool": {
+         "filter": {
+            "range": {
+               "timestamp": {
+                 "time_zone": "+05:30",        
+                 "lte": "now-30d" 
+               }
+            }
+         }
+      }
+   }
+}'
+
+#lte means less than or equal to, so the above query will return data >= currentTime to last 30 days
+
+# perform the HTTP request to Elasticsearch and save the response JSON data to a file
+#_delete_by_query to delete the documents that are fetched
+curl -u $username:$password -X POST -H "Content-Type: application/json" -d "$query" "$endpoint/$index_name/_delete_by_query" > response.json
+
+#install jq and place in /usr/local/bin, add the path to environment variables
+echo ""
+cat response.json | jq
+
+#Note : data is fetched based on timestamp
